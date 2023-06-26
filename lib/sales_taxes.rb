34833c43c4a210544a982bf66202bc818cc71afc
Sauloxd <saulotoshi@gmail.com>
2023-06-26 15:45:45 +0000

    Add round to nearest 5 and create specs

--- lib/sales_taxes.rb
+++ lib/sales_taxes.rb
@@ -1,55 +1,39 @@
-# Get data input
-require 'pry'
-
 class SalesTaxes
-  def args
-    print <<~HEREDOC
-      Add your products in the following structure:
-      [type?: imported] [quantity: string] [product_name: string] at [princing: float]
+  DEFAULT_TAX_CONFIG = {
+    exempt: [ :book, :food, :medical_product ],
+    default: 10,
+    importation: 5,
+    precision: 0.05,
+  }
 
-      Example:
-      1 imported bottle of perfume at 27.99
-      1 bottle of perfume at 18.99
-
-    HEREDOC
-    
-    args = gets
-  end 
-
-  def parse(raw_input)
-    raw_input
-      .split("\n")
-      .map { |x| x.lstrip.rstrip }
-      .filter {|x| !x.empty?}
-      .map do |line|
-        price = line.scan(/ at [0-9]+\.[0-9]+/)[0].gsub(" at ", "").to_f
-        description_tokens = line.split(/ at [0-9]+\.[0-9]+/)[0].split(/\s/)
-        quantity = description_tokens[0].to_i
-        imported = description_tokens[1] == "imported"
-        name = imported ? description_tokens.slice(2..).join(" ") : description_tokens.slice(1..).join(" ")
-        type = :other
-        type = :book if books.include?(name)
-        type = :food if foods.include?(name)
-        type = :medical_product if medical_products.include?(name)
-        {
-          type: type,
-          quantity: quantity,
-          imported: imported,
-          name: name,
-          price: price,
-        }
-      end
+  def initialize(config = nil)
+    @config = config || DEFAULT_TAX_CONFIG
   end
 
-  def books
-    ["book"]
+  def tax_product(product)
+    tax = @config[:default]
+    tax = 0 if exempt?(product)
+    tax += @config[:importation] if product.imported?
+
+    unrounded_price = (tax + 100) * product.price / 100
+
+    round_to_nearest_step(unrounded_price) * product.quantity
   end
-  def foods
-    ["chocolate bar", "boxes of chocolates", "box of chocolates"]
+
+  private
+
+  def exempt?(product) 
+    @config[:exempt].include?(product.type)
   end
-  def medical_products
-    ["packet of headache pills"]
+
+  def round_to_nearest_step(number)
+    rounded_number = number.round(3)
+    decimal_digits = rounded_number.to_s.split('.').last
+    if decimal_digits[1].to_i > 5 || decimal_digits[1].to_i == 0
+      number.round(2)
+    else
+      decimal_digits[1] = '5'
+      [rounded_number.to_s.split('.').first, decimal_digits.slice(0..1)].join('.').to_f
+    end
   end
 end
-
-# SalesTaxes.new.args
--- spec/sales_taxes_spec.rb
+++ spec/sales_taxes_spec.rb
@@ -1,57 +1,52 @@
 require_relative '../lib/sales_taxes'
+require_relative '../lib/product'
+require 'pry'
 
 RSpec.describe SalesTaxes do
-  describe "Collect arguments" do
-    context "with single line parameters" do 
-      [
-        ['2 book at 12.49', { imported: false, type: :book, quantity: 2, name: 'book', price: 12.49} ],
-        ['1 music CD at 14.99', { imported: false, type: :other, quantity: 1, name: 'music CD', price: 14.99} ],
-        ['1 chocolate bar at 0.85', { imported: false, type: :food, quantity: 1, name: 'chocolate bar', price: 0.85 } ],
-        ['1 imported box of chocolates at 10.00', { imported: true, type: :food, quantity: 1, name: 'box of chocolates', price: 10.00} ],
-        ['1 imported bottle of perfume at 47.50', { imported: true, type: :other, quantity: 1, name: 'bottle of perfume', price:  47.50} ],
-        ['1 imported bottle of perfume at 27.99', { imported: true, type: :other, quantity: 1, name: 'bottle of perfume', price: 27.99} ],
-        ['1 bottle of perfume at 18.99', { imported: false, type: :other, quantity: 1, name: 'bottle of perfume', price:  18.99} ],
-        ['1 packet of headache pills at 9.75', { imported: false, type: :medical_product, quantity: 1, name: 'packet of headache pills', price:  9.75} ],
-        ['3 imported boxes of chocolates at 11.25', { imported: true, type: :food, quantity: 3, name: 'boxes of chocolates', price:11.25 } ],
-      ].each do |(input, output)|
-        it "#{input}" do 
-          expect(described_class.new.parse(input)).to contain_exactly(output)
-        end
+  let(:subject) { described_class.new.tax_product(product) }
+  describe "Calculate tax product" do
+    context 'with varying quantity' do
+      let(:product) { Product.new(
+        imported: false, type: :book, quantity: 2, name: 'book', price: 12.49
+      ) }
+      it 'calculates final_price' do
+        expect(subject).to eq(24.98)
       end
     end
-    
-    context "with multiline parameters" do
-      let(:subject) { described_class.new.parse(input) }
-      let(:input) do 
-        "
-          2 book at 12.49
-          1 imported bottle of perfume at 47.50
-          3 somethingat at 27.51
-        "
-      end
 
-      it "parses imported, type, quantity and value properly" do
-        expect(subject).to contain_exactly({
-          imported: false,
-          type: :book,
-          quantity: 2,
-          name: 'book',
-          price: 12.49
-        }, 
-        {
-            imported: true,
-            type: :other,
-            quantity: 1,
-            name: 'bottle of perfume',
-            price: 47.50
-        },
-        {
-          imported: false,
-          type: :other,
-          quantity: 3,
-          name: 'somethingat',
-          price: 27.51
-        })
+    context 'with rounding needed' do
+      let(:product) { Product.new(
+        imported: false, type: :other, quantity: 1, name: 'bottle of perfume', price:  18.99
+      ) }
+      it 'calculates final_price' do
+        expect(subject).to eq(20.89)
+      end
+    end
+
+    context 'with imported taxable product' do
+      let(:product) { Product.new(
+        imported: true, type: :other, quantity: 1, name: 'bottle of perfume', price:  47.50
+      ) }
+      it 'calculates final_price' do
+        expect(subject).to eq(54.65)
+      end
+    end
+
+    context 'with imported taxable product' do
+      let(:product) { Product.new(
+        imported: true, type: :food, quantity: 3, name: 'boxes of chocolates', price: 11.25
+      ) }
+      it 'calculates final_price' do
+        expect(subject).to eq(35.55)
+      end
+    end
+
+    context 'with imported taxable product' do
+      let(:product) { Product.new(
+        imported: true, type: :food, quantity: 1, name: 'box of chocolates', price: 10.00
+      ) }
+      it 'calculates final_price' do
+        expect(subject).to eq(10.50)
       end
     end
   end
